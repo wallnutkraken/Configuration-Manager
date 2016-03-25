a15c681d6d4fe8cacd0feaa3376f9d90369e5558
Emile <emil44d8@edu.eal.dk>
2016-03-25 17:36:59 +0100

    added example/testing project

---
+++ ConsoleExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using ConfigManager;
+
+namespace ConsoleExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfigFile conf = new ConfigFile("file.conf");
+        }
+    }
+}
